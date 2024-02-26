7d6216dc0962d2b0159cc79730267523f519537b
Aydın Kaya <Aydinnkaya1@gmail.com>
2024-02-26 14:33:00 +0300

    Initial Commit

---
+++ MovieTicketBookingApp/MovieTicketBookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTicketBookingAppApp.swift
+//  MovieTicketBookingApp
+//
+//  Created by Aydın KAYA on 26.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieTicketBookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
